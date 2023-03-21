226e84a19bcc4b5486a6886a64e5bd274a1a0eb9
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-03-21 15:01:15 +0200

    finishing card module header

--- card/Card.h
+++ card/Card.h
@@ -2,5 +2,33 @@
 #ifndef CARD_H_
 #define CARD_H_
 
+ /* data typs and structs */
+
+typedef struct {
+
+char name[25];     // card holder name 
+char ExpDate[6];   // card expiry date mm/yy
+char PAN[20];      // card PAN
+
+}ST_cardData_t;
+
+
+typedef enum {
+
+INVALID_NAME,
+INVALID_DATE,
+INVALID_PAN,
+VALID
+
+}EN_cardError_t;
+
+
+/* Function prototypes */
+
+EN_cardError_t getCardHolderName(ST_cardData_t* card);
+EN_cardError_t getCardExpiryDate(ST_cardData_t* card);
+EN_cardError_t getCardPAN(ST_cardData_t* card);
+
+
 
 #endif // CARD_H_
