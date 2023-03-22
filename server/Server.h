bc0b1aaf2268701f6d967db97c29102e65c792f4
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-03-22 12:35:06 +0200

    finishing server module header

--- server/Server.h
+++ server/Server.h
@@ -3,5 +3,53 @@
 #ifndef SERVER_H_
 #define SERVER_H_
 
+#include "../terminal/Terminal.h"
+
+/* data typs and structs */
+
+typedef struct {
+
+}ST_accountDatabase_t;
+
+
+typedef struct {
+    
+    ST_cardData_t card_data;
+    ST_terminalData_t term_data;
+
+}ST_transactionData_t;
+
+typedef struct 
+{
+    unsigned int trans_number;
+    ST_transactionData_t trans_data;
+
+}ST_transDatabase_t;
+
+typedef enum {
+
+  ACTIVE,
+  BLOCKED,
+  INVALID_ACCOUNT
+
+}EN_accountState_t;
+
+typedef enum {
+
+  VALID_TRANS,
+  INVALID_TRANS,
+  AVAILABLE,
+  UNAVAILABLE
+
+}EN_transactionState_t;
+
+
+/* Function prototypes */
+
+EN_transactionState_t receiveTransactionData();
+EN_accountState_t isValidAccount();
+EN_transactionState_t isAmountAvailable();
+EN_transactionState_t saveTransaction();
+
 
 #endif // SERVER_H_
