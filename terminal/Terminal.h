44a0ed67e3c5fd06a1439459ce5c141b5f3d51f7
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-03-22 08:29:47 +0200

    finishing Terminal module header

--- terminal/Terminal.h
+++ terminal/Terminal.h
@@ -3,5 +3,34 @@
 #ifndef TERMINAL_H_
 #define TERMINAL_H_
 
+#include "../card/Card.h"
+#define MAX_AMOUNT    20000      // transaction limit
+
+
+/* data typs and structs */
+
+typedef struct {
+ int transAmount ;
+ int maxTransAmount;
+ char transDate[6];
+
+}ST_terminalData_t;
+
+typedef enum {
+
+ INVALID_DATA,
+ EXPIRED_CARD,
+ BELOW_MAX_AMOUNT,
+ DONE
+}EN_terminalError_t;
+
+
+/* Function prototypes */
+
+EN_terminalError_t getTransactionData(ST_terminalData_t* data);
+EN_terminalError_t isCardExpired(ST_terminalData_t *data , ST_cardData_t card);
+EN_terminalError_t getTransactionAmount(ST_terminalData_t* data);
+EN_terminalError_t isBelowMaxAmount(ST_terminalData_t *data);
+EN_terminalError_t setMaxAmount(ST_terminalData_t* data);
 
 #endif // TERMINAL_H_
