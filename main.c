2ecf52cb58c56df1d399b7e24d759e8477f6958f
mohamed_elshaarawy <m.elshaarawy96@yahoo.com>
2023-03-23 09:32:04 +0200

    update main.c
    better user interface + debug

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include  "APP/App.h"
 
 int main(void){
+ printf("--------------------------> Payment-Application <--------------------------\n \n  ");   
 while(1){
  appStart();
+ printf("-------------------------->======================<--------------------------\n \n  "); 
 }
+
 return 0;
 }
